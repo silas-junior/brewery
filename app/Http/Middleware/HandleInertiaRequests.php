b56093beb7461d7a685471001056be0b44ed5c29
Silas Junior <silas@appbee.online>
2021-04-16 11:15:10 -0300

    added inertia

---
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+
+class HandleInertiaRequests extends Middleware
+{
+
+    protected $rootView = 'app';
+
+    public function version(Request $request)
+    {
+        return parent::version($request);
+    }
+
+    public function share(Request $request)
+    {
+        return array_merge(parent::share($request), [
+            //
+        ]);
+    }
+}
