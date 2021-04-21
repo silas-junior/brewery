660054f141d68cd2ca9548f7337e6014b5aa6f13
italo <italo@appbee.online>
2021-04-21 16:57:22 -0300

    Revert "attempt to capture index and adjust route"
    This reverts commit e538cb32

--- resources/js/Shared/Pagination.vue
+++ resources/js/Shared/Pagination.vue
@@ -1,5 +1,7 @@
 <template>
-    <nav class="border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
+    <nav
+        class="border-t border-gray-200 px-4 flex items-center justify-between sm:px-0"
+    >
         <div class="-mt-px w-0 flex-1 flex">
             <inertia-link
                 v-if="data.prev_page_url"
@@ -14,13 +16,7 @@
         </div>
 
         <div class="hidden md:-mt-px md:flex">
-            <inertia-link v-for="link in clearLinks"
-                          :key="link.label"
-                          :href="link.url"
-                          :class="{ 'border-indigo-500 text-indigo-600': link.active }"
-                          class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-                {{ link.label }}
-            </inertia-link>
+            <inertia-link v-for="link in clearLinks" :key="link.label" :href="link.url" :class="{ 'border-indigo-500 text-indigo-600': link.active }" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">{{ link.label }}</inertia-link>
         </div>
 
         <div class="-mt-px w-0 flex-1 flex justify-end">
